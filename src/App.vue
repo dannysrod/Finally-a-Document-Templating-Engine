d54cac7016a6c0e7df2a5187d63e83a51bc35fee
Danny Rod <dannyrod@gmail.com>
2023-03-21 21:28:25 +1100

    modified:   index.html 	new file:   public/letter.docx 	deleted:    public/template1.docx 	modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,31 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import axios from 'axios';
-import { Docx } from 'docx';
+import Docx from 'docx';
 import Mustache from 'mustache';
 import TemplateSelector from './components/TemplateSelector.vue';
 import TemplateForm from './components/TemplateForm.vue';
+
+// This is a Vue component, which is a single-page application that is used to 
+// create a custom form based on a template. The user chooses a template from a 
+// list of available templates, and then fills out the form. The form is then 
+// submitted, and the template is filled out with the data from the form.
+//
+// The TemplateSelector component is used to select a template, and the 
+// TemplateForm component is used to fill out the form.
+//
+// The data() method sets up the available templates, and initializes the 
+// selectedTemplate and downloadLink variables.
+//
+// The selectTemplate() method is called when a template is selected from the 
+// TemplateSelector component. It sets the selectedTemplate variable to the 
+// selected template.
+//
+// The submitForm() method is called when the form is submitted. It downloads 
+// the template from the server, fills it out with the data from the form, and 
+// then sends the filled out template to the user.
 
 export default {
   components: {
@@ -23,15 +42,24 @@ export default {
     return {
       templates: [
         {
-          name: 'Template 1',
+          name: 'Letter',
           fields: [
-            { key: 'field1', label: 'Field 1' },
-            { key: 'field2', label: 'Field 2' },
+            { key: 'matterno', label: 'Matter Number' },
+            { key: 'date', label: 'Date' },
+            { key: 'op.sol', label: 'Opposing Solicitor' },
+            { key: 'op.firm', label: 'Opposing Firm' },
+            { key: 'op.firm.address', label: 'Opposing Firm Address' },
+            { key: 'op.firm.suburb', label: 'Opposing Firm Suburb' },
+            { key: 'op.firm.state', label: 'Opposing Firm State' },
+            { key: 'op.firm.postcode', label: 'Opposing Firm Postcode' },
+            { key: 'op.sol.email', label: 'Opposing Solicitor Email' },
+            { key: 'oc.surname', label: 'Own Client Surname' },
+            { key: 'op.surname', label: 'Opposing Client Surname' },
           ],
-          url: '/templates/template1.docx'
+          url: '/templates/letter.docx'
         },
-        // Add more templates here...
       ],
+        // Add more templates here...
       selectedTemplate: null,
       downloadLink: null,
       downloadName: null
